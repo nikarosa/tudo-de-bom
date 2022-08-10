1e26ff766740e96e44f8b7c5cbe6047200fd2259
alexandreps1123 <alexandre.ps1123@gmail.com>
2022-08-10 20:18:41 -0300

    Spring Initializr

---
+++ src/main/java/com/tudodebom/TudoDeBomApplication.java
@@ -0,0 +1,13 @@
+package com.tudodebom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TudoDeBomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TudoDeBomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tudodebom/TudoDeBomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tudodebom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TudoDeBomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
